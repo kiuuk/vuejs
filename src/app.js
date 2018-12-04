2a0e22dffa007df8a2af4b92786910767abaf567
Kay Ha <kiwook.ha@gmail.com>
2018-12-04 23:02:30 +0900

    list binding

--- src/app.js
+++ src/app.js
@@ -5,9 +5,37 @@ window.$ = window.jQuery = $;
 
 $(function(){
   new Vue({
-      el: '#app',
-      data: {
-        message: 'Hello Vue!'
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+});
+
+$(function(){
+  new Vue(
+  {
+    el: '#app-list',
+    data() {
+      return {
+        items: [
+          {
+            id: 1,
+            title: 'img-1',
+            images: 'https://dummyimage.com/120x120/efefef/999999&text=1'
+          },
+          {
+            id: 2,
+            title: 'img-2',
+            images: 'https://dummyimage.com/120x120/efefef/999999&text=2'
+          },
+          {
+            id: 3,
+            title: 'img-3',
+            images: 'https://dummyimage.com/120x120/efefef/999999&text=3'
+          }
+        ]
       }
-    });
+    }
+  })
 });
