17745e6dc9b1ef8e853c2b2f7e5d9967aff3462f
Kay Ha <kiwook.ha@gmail.com>
2019-02-17 22:39:57 +0900

    add suggest box

--- src/components/searchbox.vue
+++ src/components/searchbox.vue
@@ -4,6 +4,11 @@
       <div class="form-group">
         <label for="searchInput">Search Suggestion</label>
         <input type="text" class="form-control" id="searchInput" placeholder>
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
+          <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+          <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+        </div>
       </div>
     </form>
   </div>
@@ -13,5 +18,5 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
 </style>
