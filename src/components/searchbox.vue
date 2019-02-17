93bb1d02c0a085a3e991933d981d5e9cf9580237
Kay Ha <kiwook.ha@gmail.com>
2019-02-17 22:59:23 +0900

    add axios data

--- src/components/searchbox.vue
+++ src/components/searchbox.vue
@@ -5,9 +5,12 @@
         <label for="searchInput">Search Suggestion</label>
         <input type="text" class="form-control" id="searchInput" placeholder>
         <div class="list-group">
-          <a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
-          <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-          <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+          <a
+            href="#"
+            class="list-group-item list-group-item-action"
+            v-for="user in users"
+            :key="user.id"
+          >{{user.name}}</a>
         </div>
       </div>
     </form>
@@ -15,7 +18,27 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        this.users = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  }
+};
 </script>
 
 <style scoped>
