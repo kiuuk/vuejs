a44978abb7816d3f1c7d02f92019fb20d079d58d
Kay Ha <kiwook.ha@gmail.com>
2019-02-18 11:39:39 +0900

    add filter

--- src/components/searchbox.vue
+++ src/components/searchbox.vue
@@ -3,14 +3,14 @@
     <form>
       <div class="form-group">
         <label for="searchInput">Search Suggestion</label>
-        <input type="text" class="form-control" id="searchInput" placeholder>
-        <div class="list-group">
+        <input type="text" class="form-control" id="searchInput" v-model="search" @input="onChange">
+        <div class="list-group" v-show="isOpen">
           <a
             href="#"
             class="list-group-item list-group-item-action"
-            v-for="user in users"
-            :key="user.id"
-          >{{user.name}}</a>
+            v-for="(result,idx) in results"
+            :key="idx"
+          >{{result.name}}</a>
         </div>
       </div>
     </form>
@@ -23,7 +23,10 @@ import axios from "axios";
 export default {
   data() {
     return {
-      users: []
+      search: "",
+      users: [],
+      results: [],
+      isOpen: false
     };
   },
   mounted() {
@@ -37,6 +40,17 @@ export default {
         this.errored = true;
       })
       .finally(() => (this.loading = false));
+  },
+  methods: {
+    onChange() {
+      this.isOpen = true;
+      this.filterResults();
+    },
+    filterResults() {
+      this.results = this.users.filter(
+        user => user.name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+      );
+    }
   }
 };
 </script>
